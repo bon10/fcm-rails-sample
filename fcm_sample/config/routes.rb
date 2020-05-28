1e9b61767972b3ab897fe69b6dc953c3a0fea035
bon10 <bontyun@gmail.com>
2020-05-29 02:44:57 +0900

    fix routes

--- fcm_sample/config/routes.rb
+++ fcm_sample/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  'users#index'
+
+  #ユーザー一覧を表示
+  get '/users', to: 'users#index'
+
 end
