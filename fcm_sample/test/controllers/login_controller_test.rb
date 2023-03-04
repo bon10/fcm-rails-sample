9237a92fe6cd9e4e854a45f5b3aaea4f6a5422c1
bon <bontyun@gmail.com>
2023-03-05 04:59:51 +0900

    fix

--- fcm_sample/test/controllers/login_controller_test.rb
+++ fcm_sample/test/controllers/login_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class LoginControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get login_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get login_create_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get login_destroy_url
     assert_response :success
   end
